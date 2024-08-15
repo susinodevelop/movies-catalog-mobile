8063e5f5a087a32813553220b808042a41dff529
Suso <susinodevelop@gmail.com>
2024-08-15 16:01:47 +0200

    Correcciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./presentation/screens/home/HomeScreen";
+import DetailScreen from "./presentation/screens/details/DetailScreen";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const App = ()=>{
+export type RootStackParams = {
+  Home: undefined;
+  Details: {
+    movieId: number;
+  };
+};
 
-    return (
-        <View style={styles.container}>
-            <Text>App</Text>
-        </View>
-    )
-}
+const Stack = createStackNavigator<RootStackParams>();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default App;
