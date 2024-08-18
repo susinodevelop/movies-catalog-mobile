878b5cbe85d5302ed85d3326a556bec32af0f6b1
Suso <susinodevelop@gmail.com>
2024-08-18 20:18:09 +0200

    Estiliza status bar y navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar as NativeStatusBar,
+  StyleSheet,
+} from "react-native";
 import HomeScreen from "./presentation/screens/home/HomeScreen";
 import DetailScreen from "./presentation/screens/details/DetailScreen";
 import { createStackNavigator } from "@react-navigation/stack";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from "expo-navigation-bar";
 
 export type RootStackParams = {
   Home: undefined;
@@ -15,18 +24,35 @@ export type RootStackParams = {
 const Stack = createStackNavigator<RootStackParams>();
 
 const App = () => {
+  React.useEffect(() => {
+    NavigationBar.setBackgroundColorAsync("black");
+    NavigationBar.setButtonStyleAsync("light"); 
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="light" backgroundColor="black" />
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? NativeStatusBar.currentHeight : 0,
+  },
+});
 
 export default App;
